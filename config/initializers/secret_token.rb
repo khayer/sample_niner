6fa309b703e2711933169692adfd3e42b2b228c4
Katharina Hayer <katharinaehayer@gmail.com>
2012-02-08 16:24:43 -0500

    How it starts

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleNiner::Application.config.secret_token = 'abefe26de5aeab338847e28960f9f30a6b0baaa1581b9963afbe52a1b097bcaee4ac9fc20a11c284d0b0bf87d4a9ad15d492a6599a071d8e0ab1ff81d8c2a9e0'
